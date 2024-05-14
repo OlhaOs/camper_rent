e244e4a78d8c299042b8735ec14d59de83f150b5
OlhaOs <olalaadka@gmail.com>
2024-05-14 19:32:30 +0300

    routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import './App.css';
+import { Link, Route, Routes } from 'react-router-dom';
+import css from './App.module.css';
+import { HomePage } from './pages/HomePage';
+import { Catalog } from './pages/Catalog';
+import { Favorites } from './pages/Favorites';
 
-function App() {
-  return <div>Camper</div>;
-}
-
-export default App;
+export const App = () => {
+  return (
+    <>
+      <Link to='/' className={css.Navlink}>
+        Home
+      </Link>
+      <Link to='/catalog' className={css.Navlink}>
+        Catalog
+      </Link>
+      <Link to='/favorites'>Favorites</Link>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/catalog' element={<Catalog />} />
+        <Route path='/favorites' element={<Favorites />} />
+        <Route path='*' element={<div>Error 404</div>} />
+      </Routes>
+    </>
+  );
+};
