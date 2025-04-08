52e4025fdb9828dd91173a7f9d6b164ef4fff3cc
OlhaOs <olalaadka@gmail.com>
2025-04-08 20:47:16 +0300

    add homepage

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,8 +1,23 @@
 import css from './HomePage.module.css';
+import { Link } from 'react-router-dom';
+import homeImage from '../../image/home_image.jpg';
 export const HomePage = () => {
   return (
     <>
-      <section className={css.homePageContainer}>HomePage</section>
+      <section className={css.heroWrapper}>
+        <img src={homeImage} alt='a camper example' className={css.heroImage} />
+        <div className={css.heroInfo}>
+          <h1 className={css.title}>Explore world – the road is yours</h1>
+          <h2 className={css.subtitle}>
+            Rent a camper and set out on your dream adventure.
+            <br />
+            Camp your way – wild, free, unforgettable.
+          </h2>
+          <Link to='/catalog'>
+            <button className={css.ctaButton}>Rent now </button>
+          </Link>
+        </div>
+      </section>
     </>
   );
 };
