c67832421e929fcae349bc95bf0fcf8941c13315
OlhaOs <olalaadka@gmail.com>
2025-04-09 13:23:34 +0300

    add 494

--- src/components/FavoritesList/FavoritesList.jsx
+++ src/components/FavoritesList/FavoritesList.jsx
@@ -1,13 +1,14 @@
 import { CatalogItem } from '../CatalogItem/CatalogItem';
 import css from './FavoritesList.module.css';
 import { useSelector } from 'react-redux';
+import icons from '../../icon/icons.svg';
 
 export const FavoritesList = () => {
   const favoritesCampers = useSelector(state => state.favorites.favorites);
 
   return (
     <>
-      {favoritesCampers.length > 0 && (
+      {favoritesCampers.length > 0 ? (
         <>
           <ul className={css.catalogList}>
             {favoritesCampers.map(item => (
@@ -15,6 +16,16 @@ export const FavoritesList = () => {
             ))}
           </ul>
         </>
+      ) : (
+        <div className={css.wrapper}>
+          <svg className={css.heartIcon}>
+            <use href={icons + '#icon-heart'} />
+          </svg>
+          <h2 className={css.title}>You haven't added any favorite campers.</h2>
+          <p className={css.text}>
+            Add your favorite campers to easily find them here.
+          </p>
+        </div>
       )}
     </>
   );
